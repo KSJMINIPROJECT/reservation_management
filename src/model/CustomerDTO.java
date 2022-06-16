18f035cd80226c44f0cdfda9e9127b9e65e2f9b3
Ye Yeong Kwak <aesop0817@gmail.com>
2022-06-16 18:10:36 +0900

    Add CustomerDTO, ReservationDTO

--- src/model/CustomerDTO.java
+++ src/model/CustomerDTO.java
@@ -1,5 +1,71 @@
 package model;
 
 public class CustomerDTO {
+	private String customerId;
+	private int headCount;
+	private String customerName;
+	private String phoneNumber;
+	
+public CustomerDTO() {}
 
+public CustomerDTO(String customerId, String customerName, int headCount,  String phoneNumber) {
+	super();
+	this.customerId = customerId;
+	this.headCount = headCount;
+	this.customerName = customerName;
+	this.phoneNumber = phoneNumber;
+}
+
+public String getCustomerId() {
+	return customerId;
+}
+
+public void setCustomerId(String customerId) {
+	this.customerId = customerId;
+}
+public String getCustomerName() {
+	return customerName;
+}
+
+public void setCustomerName(String customerName) {
+	this.customerName = customerName;
+}
+public int getHeadCount() {
+	return headCount;
+}
+
+public void setHeadCount(int headCount) {
+	this.headCount = headCount;
+}
+
+
+
+public String getPhoneNumber() {
+	return phoneNumber;
+}
+
+public void setPhoneNumber(String phoneNumber) {
+	this.phoneNumber = phoneNumber;
+}
+
+@Override
+public String toString() {
+//	return "CustomerDTO [customerId=" + customerId + ", headCount=" + headCount + ", customerName=" + customerName
+//			+ ", phoneNumber=" + phoneNumber + "]";
+	StringBuilder builder = new StringBuilder();
+	builder.append("[고객 ID : ");
+	builder.append(customerId);
+	builder.append(", 고객 이름 : ");
+	builder.append(customerName);
+	builder.append(", 최대 인원 : ");
+	builder.append(customerId);
+	builder.append(", 고객 연락처 : ");
+	builder.append(phoneNumber);
+	return builder.toString();
+}
+
+
+	
+	
+	
 }
--- src/model/ReservationDTO.java
+++ src/model/ReservationDTO.java
@@ -1,5 +1,75 @@
 package model;
 
-public class ReservationDTO {
+import java.sql.Date;
 
+public class ReservationDTO {
+	
+	private int reservationId;
+	private String customerId;
+	private int roomId;
+	private Date startDate;
+	private Date endDate;
+	
+	public ReservationDTO() {}
+		
+	public ReservationDTO(int reservationId, String customerId, int roomId, Date startDate, Date endDate) {
+		super();
+		this.reservationId = reservationId;
+		this.customerId = customerId;
+		this.roomId = roomId;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
+	public int getReservationId() {
+		return reservationId;
+	}
+	public void setReservationId(int reservationId) {
+		this.reservationId = reservationId;
+	}
+	public String getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
+	}
+	public int getRoomId() {
+		return roomId;
+	}
+	public void setRoomId(int roomId) {
+		this.roomId = roomId;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	@Override
+	public String toString() {
+//		return "ReservationDTO [reservationId=" + reservationId + ", customerId=" + customerId + ", roomId=" + roomId
+//				+ ", startDate=" + startDate + ", endDate=" + endDate + "]";
+		
+		StringBuilder builder = new StringBuilder();
+		builder.append("[예약 ID : ");
+		builder.append(reservationId);
+		builder.append(", 고객  ID : ");
+		builder.append(customerId);
+		builder.append(", 방번호 : ");
+		builder.append(roomId);
+		builder.append(", 숙박 시작 날짜 : ");
+		builder.append(startDate);
+		builder.append(", 숙박 종료 날짜 : ");
+		builder.append(endDate);
+		return builder.toString();
+	}
+	
+	
+		
 }
