d50e21c2dce05e2ef9ee1cca2185530128e62c45
hyeonchaeshin <scsoks@gmail.com>
2022-06-17 10:39:11 +0900

    Room DAO, RoomDTO, RoomService

--- src/service/RoomServiceInterface.java
+++ src/service/RoomServiceInterface.java
@@ -1,12 +1,13 @@
 package service;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import model.RoomDTO;
 
 public interface RoomServiceInterface {
-	public ArrayList<RoomDTO> allRoom();
-	public RoomDTO selectRoom(int roomId);
+	public ArrayList<RoomDTO> allRoom() throws SQLException;
+	public RoomDTO selectRoom(int roomId) throws SQLException;
 	// 날짜로 빈방 찾기
 	
 	public boolean addRoom(RoomDTO newRoom);
