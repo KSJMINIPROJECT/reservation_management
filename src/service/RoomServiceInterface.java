11905fe75aa97df291fa3c98cc9a8e10d4cec315
Gloryjeong <vles153@naver.com>
2022-06-17 11:13:38 +0900

    Add startView

--- src/service/RoomServiceInterface.java
+++ src/service/RoomServiceInterface.java
@@ -1,6 +1,5 @@
 package service;
 
-import java.sql.Date;
 import java.util.ArrayList;
 
 import model.RoomDTO;
@@ -8,8 +7,6 @@ import model.RoomDTO;
 public interface RoomServiceInterface {
 	public ArrayList<RoomDTO> allRoom();
 	public RoomDTO selectRoom(String roomId);
-	// 날짜로 빈방 찾기
-	public ArrayList<RoomDTO> selectEmptyRoom(Date date);
 	public boolean addRoom(RoomDTO newRoom);
 	public boolean updateRoom(String roomId,int price);
 	public boolean deleteRoom(String roomId);
